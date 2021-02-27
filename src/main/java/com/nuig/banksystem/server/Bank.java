ad07fc6f8296d60521c3fde30e6640af41efe9d9
MohamedMoustafaNUIG <mohamedmoustafaofficial@gmail.com>
2021-02-27 18:27:27 +0000

    filled in needed methods with minimal reqs and created main method for server

--- src/main/java/com/nuig/banksystem/server/Bank.java
+++ src/main/java/com/nuig/banksystem/server/Bank.java
@@ -11,20 +11,46 @@ import org.joda.money.Money;
 public class Bank implements BankInterface {
     private List<Account> accounts; // users accounts
     public Bank() throws RemoteException{}
-
-    public void deposit(int account, Money amount) throws RemoteException, InvalidSession {
-        // implementation code
+    
+    public long login(String username, String password) throws RemoteException, InvalidLogin{return 1;}
+    public void deposit(int accountnum, Money amount, long sessionID) throws RemoteException, InvalidSession{}
+    public void withdraw(int accountnum, Money amount, long sessionID) throws RemoteException, InvalidSession{}
+    public Money getBalance(int accountnum, long sessionID) throws RemoteException, InvalidSession{
+        Money m = Money.parse("USD 12.00");
+        return m;
     }
-    public void withdraw(int account, Money amount) throws RemoteException, InvalidSession {
-        // implementation code
-    }
-    public Money getBalance(int account) throws RemoteException, InvalidSession {
-        // implementation code
-    }
-    public Statement getStatement(Date from, Date to) throws RemoteException, InvalidSession {
+    public Statement getStatement(Date from, Date to, long sessionID) throws RemoteException, InvalidSession {
         return new BankStatement();
     }
     public static void main(String args[]) throws Exception {
-        // initialise Bank server - see sample code in the notes and online RMI tutorials for details
-    }
+        try
+            {
+              // First reset our Security manager
+              if (System.getSecurityManager() == null) {
+                    System.setSecurityManager(new SecurityManager());
+                    System.out.println("Security manager set");
+                }
+
+              // Create an instance of the local object
+              BankInterface b = new Bank();
+              System.out.println("Instance of Bank Server created");
+              BankInterface stub = (BankInterface) UnicastRemoteObject.exportObject(b, 0);
+              System.out.println("stub created");
+
+              // Put the server object into the Registry
+              Registry registry = LocateRegistry.getRegistry();
+              System.out.println("registry reference gotten");
+
+              System.out.println(registry.list());
+
+              registry.rebind("Bank", stub);
+              System.out.println("Name rebind completed");
+
+              System.out.println("Server ready for requests!");
+            }
+        catch(Exception exc)
+            {
+              System.out.println("Error in main - " + exc.toString());
+            }
+        }
 }
--- src/main/java/com/nuig/banksystem/server/BankStatement.java
+++ src/main/java/com/nuig/banksystem/server/BankStatement.java
@@ -4,14 +4,15 @@ package com.nuig.banksystem.server;
 import com.nuig.banksystem.server.Statement;
 import com.nuig.banksystem.server.Transaction;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import java.util.Date;
 import java.util.List;
 
 public class BankStatement implements Statement, Serializable{
-    public int getAccountnum();  // returns account number associated with this statement
-    public Date getStartDate(); // returns start Date of Statement
-    public Date getEndDate(); // returns end Date of Statement
-    public String getAccoutName(); // returns name of account holder
-    public List<Transaction> getTransactions(); // return list of transactions included in this statement  
+    public int getAccountnum(){return 1;}  // returns account number associated with this statement
+    public Date getStartDate(){return new Date();} // returns start Date of Statement
+    public Date getEndDate(){return new Date();} // returns end Date of Statement
+    public String getAccoutName(){return "account 1";} // returns name of account holder
+    public List<Transaction> getTransactions(){return new ArrayList<Transaction>();} // return list of transactions included in this statement  
 }
